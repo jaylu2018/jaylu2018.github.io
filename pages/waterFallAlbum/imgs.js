79280af7332c2f4fa3550b947ffaa309f5d5874a
卢育恒 <luyuheng@luyuhengdeMacBook-Pro.local>
2019-02-10 12:48:24 +0800

    Site updated: 2019-02-10 12:48:24

---
+++ pages/waterFallAlbum/imgs.js
@@ -0,0 +1,16 @@
+var data = {
+  "2018-04-23": [
+    "https://src.wangriyu.wang/images/album/20180423/1.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/2.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/3.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/4.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/6.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/296506.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/655517.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/834017.png",
+    "https://src.wangriyu.wang/images/album/20180423/1516117848411.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/1516117960078.jpg",
+    "https://src.wangriyu.wang/images/album/20180423/fish.jpg",
+    "https://src.wangriyu.wang/images/album/20180520/love.png"
+  ]
+};
